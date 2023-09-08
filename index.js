dd3c44c7e57f1a3c460a378a46e46dba327e4ec7
Renata Reinartz <rcreinartz@gmail.com>
2023-09-07 22:55:01 -0600

    added-cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import express from 'express';
+import cors from 'cors';
 
 const prisma = new PrismaClient();
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
